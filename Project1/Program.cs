0241b6888959f99fd150cbe8ec6737411d04936c
Iván <ivanjose_04@hotmail.com>
2023-05-31 20:31:31 -0400

    first commit

---
+++ Project1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Rectangle newObject = new Rectangle(10, 10);
+
+        double perimetre = newObject.Perimetre();
+        double surface = newObject.Surface();
+
+        newObject.Afficher();
+
+        Console.WriteLine("Le perimetre est: {0} et la surface est: {1}", perimetre, surface);
+    }
+}
---
+++ Project1/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+
+public class Rectangle
+{
+    private double largeur;
+    private double longueur;
+
+    public Rectangle(double largeur, double longueur)
+    {
+        this.largeur = largeur;
+        this.longueur = longueur;
+    }
+
+    public double Perimetre()
+    {
+        return (this.largeur + this.longueur) * 2;
+    }
+
+    public double Surface()
+    {
+        return (this.largeur * this.longueur);
+    }
+
+    public void Afficher()
+    {
+        Console.WriteLine("Le largeur est: {0} et le longueur est: {1}", this.largeur, this.longueur);
+    }
+}
